b9494c86367fad4acf983dbea69cc8b73e9bc2fe
Colin Christy <chriscol@iu.edu>
2026-04-10 01:44:24 -0400

    Set up basic input loop

--- main.rb
+++ main.rb
@@ -4,11 +4,39 @@ require_relative "classes/text_chunker"
 require_relative "classes/embedding_generator"
 require_relative "classes/vector_store"
 
+# Initialize the components of the RAG system
 docLoader = DocumentLoader.new
-text = docLoader.loadDocument("C455_S26_Syllabus.pdf")
-
 textCleaner = TextCleaner.new
-text = textCleaner.cleanText(text)
+textChunker = TextChunker.new(100, 20)
+embedGen = EmbeddingGenerator.new
+vectorStore = VectorStore.new
 
-text = docLoader.loadDocument("C311_S26_Syllabus.txt")
-text = textCleaner.cleanText(text)
+puts "DocuAnswer RAG 1.0"
+puts "Enter q to quit."
+
+# Start input loop
+input = ""
+while true
+  print "Filepath: "
+  input = gets.chomp
+  if input == "q"
+    abort "Quitting..."
+    break
+  end
+
+  if !File.exist?(input)
+    puts "#{input} is not a valid filepath."
+    next
+  end
+
+  extension = File.extname(input)
+  if extension != ".txt" && extension != ".pdf"
+    puts "#{input} is not of the .txt or .pdf file type."
+    next
+  end
+
+  # input has been verified as a valid filepath, now we process the document
+  text = docLoader.loadDocument(input)
+  text = textCleaner.cleanText(text)
+  chunks = textChunker.chunk_text(text)
+end
