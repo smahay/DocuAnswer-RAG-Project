12796e55b71511e75df84fef56ca9489162bf2b1
Shivansh Mahay <shivanshmahay@Shivanshs-MacBook-Pro.local>
2026-04-10 16:30:58 -0400

    Made changes to make document loader.

--- classes/document_loader.rb
+++ classes/document_loader.rb
@@ -1,31 +1,30 @@
 require "pdf-reader"
 
 class DocumentLoader
-  def load_document(fileName)
-    puts "Loading document '#{fileName}'..."
-    text = ""
-    extension = File.extname(fileName)
+  def load_document(file_name)
+    if file_name.nil? || file_name.strip.empty?
+      raise "Please provide a file name."
+    end
+
+    extension = File.extname(file_name).downcase
+    if !File.exist?(file_name)
+      raise "File not found: #{file_name}"
+    end
+
     if extension == ".txt"
-      text = File.read(fileName)
+      text = File.read(file_name)
     elsif extension == ".pdf"
-      reader = PDF::Reader.new(fileName)
-
-      # Get text from each page as array, then join into single string
-      text = reader.pages.map(&:text).join("\n") 
+      reader = PDF::Reader.new(file_name)
+      text = reader.pages.map(&:text).join("\n")
+    else
+      raise "Only .txt and .pdf files are supported."
     end
 
-    # Check to see if any content was read
     text = text.strip
-    if text == ""
-      throw "DocumentLoader failed to find text in '#{fileName}."
+    if text.empty?
+      raise "DocumentLoader could not find text in '#{file_name}'."
     end
 
-    puts "Finished reading document."
-    
-    # puts "Text found: ---------------------------------------------------------"
-    # puts text
-    # puts "---------------------------------------------------------------------"
-    
-    return text
+    text
   end
 end
